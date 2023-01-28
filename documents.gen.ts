cd5c757707f63686e15a8073b52c20237130710a
sebekerga <sebekerka@gmail.com>
2023-01-28 23:14:48 +0600

    added documents manifest to repo for auto deploy

---
+++ documents.gen.ts
@@ -0,0 +1,11 @@
+// DO NOT EDIT. This file is generated.
+// This file SHOULD be checked into source version control.
+// This file is automatically updated during development when running `dev.ts`.
+
+import $0 from "./documents/deno_resume.tsx";
+
+const manifest = [
+  $0,
+];
+
+export default manifest;
